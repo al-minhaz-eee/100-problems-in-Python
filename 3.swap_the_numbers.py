0afecec5be9e41411a4ec3ce26c991c36199e2b6
alminhaz04 <alminhaz04@gmail.com>
2024-07-25 06:58:35 +0600

    3. User will input (2numbers).Write a program to swap the numbers

---
+++ 3.swap_the_numbers.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number : " ))
+num2 = int(input("Enter second number : " ))
+
+# swaping
+temp = num1
+num1 = num2
+num2 = temp
+print("Here is swaping ", num1,"AND ", num2)
+
+num3 = int(input("Enter third number : " )git )
+num4 = int(input("Enter forth number : " ))
+num3, num4 = num4, num3
+print("Here is swaping ",num3, "and ", num4)
