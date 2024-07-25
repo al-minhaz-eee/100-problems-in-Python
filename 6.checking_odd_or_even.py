1319ed193fc9fb89fd32ae6211298bd809d2b4cb
alminhaz04 <alminhaz04@gmail.com>
2024-07-25 08:29:23 +0600

    6. Write a program that will tell whether the number entered by the user is odd or even

--- 6.checking_odd_or_even.py
+++
@@ -1,9 +0,0 @@
-"""
- 6. Write a program that will tell whether the number entered by the
- user is odd or even.
-"""
-n = int(input("Enter a number : "))
-if n%2 ==0:
-    print(n, " is even number")
-else:
-    print(n, " is odd number")
