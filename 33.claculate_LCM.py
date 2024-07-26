f40b9352d81d1c0d51cb66d23c140ca750f4ba22
alminhaz04 <alminhaz04@gmail.com>
2024-07-26 13:58:40 +0600

    32.User will provide 2 numbers you have to find the HCF of those 2  numbers

---
+++ 33.claculate_LCM.py
@@ -0,0 +1,15 @@
+"""
+33.User will provide 2 numbers you have to find the by LCM of those 2
+ numbers
+"""
+def find_hcf(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+def find_lcm(a, b):
+    lcm = abs(a*b)//find_hcf(a, b)
+    return lcm
+
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter last number "))
+print(find_lcm(num1, num2))
