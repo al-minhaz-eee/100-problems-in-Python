8c5ab639989a9762cd8733a5bc313b085c6a563e
alminhaz04 <alminhaz04@gmail.com>
2024-07-26 14:01:41 +0600

    commit it

--- 33.claculate_LCM.py
+++ 33.claculate_LCM.py
@@ -1,5 +1,5 @@
 """
-33.User will provide 2 numbers you have to find the by LCM of those 2
+33.User will provide  2 numbers you have to find the by LCM of those 2
  numbers
 """
 def find_hcf(a, b):
