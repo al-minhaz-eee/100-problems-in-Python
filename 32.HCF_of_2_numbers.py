7dcece9de48f9c1924415ae43df3d303dca1bd20
alminhaz04 <alminhaz04@gmail.com>
2024-07-26 13:48:16 +0600

    32.User will provide 2 numbers you have to find the HCF of those 2  numbers

---
+++ 32.HCF_of_2_numbers.py
@@ -0,0 +1,12 @@
+"""
+ 32.User will provide 2 numbers you have to find the HCF of those 2
+ numbers
+"""
+def find_hcf(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+num1 = int(input("Enter last number "))
+num2 = int(input("Enter last number "))
+print(find_hcf(num1, num2))
