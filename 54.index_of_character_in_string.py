dac1bcf4a7f1228a8d53caba28a07fb6b5e5c732
alminhaz04 <alminhaz04@gmail.com>
2024-07-27 10:44:48 +0600

    commit it

---
+++ 54.index_of_character_in_string.py
@@ -0,0 +1,19 @@
+"""
+ 54.Find the index position of a particular character in another string.
+
+"""
+def count_index(string, ch):
+    count=0
+    for char in string:
+        count += 1
+        if char == " ":
+            continue
+        if char == ch:
+            yield count-1
+
+sen = input('Enter a sentence ')
+ch = input('Enter a character for index position ')
+positions_ch = count_index(sen, ch)
+print(f"index(s) of {ch} : ", end="")
+for position in positions_ch:
+    print(position, end=" ")
